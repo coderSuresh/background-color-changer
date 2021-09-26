24443904f01378739b705db8203b0651aa93295f
Coder Suresh <48799677+coderSuresh@users.noreply.github.com>
2021-09-26 18:31:55 +0545

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const body = document.getElementById("body");;
+const button = document.getElementById("changer");
+
+const colors = ["red", "green", "gray", "pink", "skyblue"];
+
+function changeColor() {
+    const position = Math.floor(Math.random() * colors.length);
+
+    body.style.background = colors[position];
+}
